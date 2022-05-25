3f1be27e067936540d9538b4f514df5154e27041
flave1 <favouremmanuel433@gmail.com>
2022-05-25 16:19:18 +0100

    ..

--- src/router/spm-path-locations.js
+++ src/router/spm-path-locations.js
@@ -22,7 +22,7 @@ export const classLocations = {
   sessionClassList: "/dashboard/smp-class/session-class",
   sessionClassEdit: "/dashboard/smp-class/edit-session-class",
   sessionClassAdd: "/dashboard/smp-class/add-session-class",
-  sessionClassDetail: "/dashboard/smp-class/details",
+  sessionClassDetail: "/dashboard/smp-class/session-class-detail",
 };
 
 export const sessionLocations = {
