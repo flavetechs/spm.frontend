3eb105f14f57dd342532b12fb570476763b3a460
flave1 <favouremmanuel433@gmail.com>
2022-05-28 12:37:54 +0100

    base ul

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // 'https://localhost:44373/',
 const axiosInstance = axios.create({
-    baseURL: 'http://sub.vendtechsl.com/',
+    baseURL: 'http://flavetech-001-site1.etempurl.com/',
     headers: {
         Authorization: '',
     },
