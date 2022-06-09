158359b3827897b15d49cb4043c44a4ad307359f
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2022-06-09 15:38:58 +0100

    #SP-41 feat(smp): add-session-class-subject-score

--- src/components/smp-session/PromotionSetup.js
+++ src/components/smp-session/PromotionSetup.js
@@ -10,17 +10,23 @@ import {
 import { Link, useHistory, useLocation } from "react-router-dom";
 import Card from "../Card";
 import { useDispatch, useSelector } from "react-redux";
-import { Formik, Field } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Field } from "formik";
+import * as Yup from "yup";
 import {
-  respondToDeleteDialog,
+  respondDialog,
   showErrorToast,
-  showSingleDeleteDialog,
+  showHideDialog,
 } from "../../store/actions/toaster-actions";
 import { getAllSessionClasses } from "../../store/actions/class-actions";
 import { sessionLocations } from "../../router/spm-path-locations";
-import { getAllPromotionList, promoteStudent } from "../../store/actions/promotion-actions";
-import { fetchSingleSession, getActiveSession } from "../../store/actions/session-actions";
+import {
+  getAllPromotionList,
+  promoteStudent,
+} from "../../store/actions/promotion-actions";
+import {
+  fetchSingleSession,
+  getActiveSession,
+} from "../../store/actions/session-actions";
 
 const PromotionSetup = () => {
   //VARIABLE DECLARATIONS
@@ -29,8 +35,8 @@ const PromotionSetup = () => {
   const locations = useLocation();
   const [showDeleteButton, setDeleteButton] = useState(true);
   const [showCheckBoxes, setShowCheckBoxes] = useState(false);
-  const [classToPromoteTo, setClassToPromoteTo] = useState('');
-  const [classToPromote, setClassToPromote] = useState('');
+  const [classToPromoteTo, setClassToPromoteTo] = useState("");
+  const [classToPromote, setClassToPromote] = useState("");
   //VARIABLE DECLARATIONS
 
   // ACCESSING STATE FROM REDUX STORE
@@ -46,30 +52,38 @@ const PromotionSetup = () => {
 
   //VALIDATIONS SCHEMA
   const validation = Yup.object().shape({
-
-    classId: Yup.string()
-      .required('Start Year is required'),
+    classId: Yup.string().required("Start Year is required"),
   });
   //VALIDATIONS SCHEMA
 
-  const handlePromotion = (sessionClassId) => {
-    
-   
-  }
-
-
+  const handlePromotion = (sessionClassId) => {};
 
   React.useEffect(() => {
-    getActiveSession()(dispatch)
-    getAllPromotionList()(dispatch)
+    getActiveSession()(dispatch);
+    getAllPromotionList()(dispatch);
   }, []);
 
   React.useEffect(() => {
     getAllSessionClasses(activeSession?.sessionId)(dispatch);
   }, [activeSession]);
 
-console.log('classtopromoteto',classToPromoteTo)
-console.log('classtopromote',classToPromote)
+  React.useEffect(() => {
+    if (deleteDialogResponse === "continue") {
+      if (!classToPromoteTo) {
+        showErrorToast("No class selected to be promoted")(dispatch);
+      } else {
+        promoteStudent(classToPromote, classToPromoteTo)(dispatch);
+        showHideDialog(false, null)(dispatch);
+        respondDialog("")(dispatch);
+      }
+    }
+    return () => {
+      respondDialog("")(dispatch);
+    };
+  }, [deleteDialogResponse]);
+
+  console.log("classtopromoteto", classToPromoteTo);
+  console.log("classtopromote", classToPromote);
   return (
     <>
       <div>
@@ -91,11 +105,7 @@ console.log('classtopromote',classToPromote)
                   >
                     <thead className="text-dark">
                       <tr className="ligth">
-                        <th>
-                          {
-                            "S/No"
-                          }
-                        </th>
+                        <th>{"S/No"}</th>
                         <th className="text-center">Previous Class</th>
                         <th className="text-center">
                           Total Student <br /> in prev class
@@ -119,12 +129,12 @@ console.log('classtopromote',classToPromote)
                     </thead>
                     <Formik
                       initialValues={{
-                        classId: '',
+                        classId: "",
                       }}
                       validationSchema={validation}
-                      onSubmit={values => {
+                      onSubmit={(values) => {
                         // createSession(values)(dispatch)
-                        promoteStudent(values)(dispatch)
+                        promoteStudent(values)(dispatch);
                       }}
                     >
                       {({
@@ -134,13 +144,18 @@ console.log('classtopromote',classToPromote)
                         values,
                         touched,
                         errors,
-                        isValid }) => (
+                        isValid,
+                      }) => (
                         <tbody>
                           {promotionList.map((item, idx) => (
                             <tr key={idx}>
                               <td className="h4">{idx + 1}</td>
-                              <td className="h5 text-center">{item.sessionClassName}</td>
-                              <td className="h4 text-center">{item.totalStudentsInClass}</td>
+                              <td className="h5 text-center">
+                                {item.sessionClassName}
+                              </td>
+                              <td className="h4 text-center">
+                                {item.totalStudentsInClass}
+                              </td>
                               <td className="h2 text-center">
                                 <OverlayTrigger
                                   placement="top"
@@ -158,7 +173,9 @@ console.log('classtopromote',classToPromote)
                                     data-original-title="Details"
                                     to={`${sessionLocations.promotionPassedList}?sessionClassId=${item.sessionClassId}`}
                                   >
-                                    <Badge bg="success">{item.totalStudentsPassed}</Badge>
+                                    <Badge bg="success">
+                                      {item.totalStudentsPassed}
+                                    </Badge>
                                   </Link>
                                 </OverlayTrigger>
                               </td>
@@ -179,31 +196,42 @@ console.log('classtopromote',classToPromote)
                                     data-original-title="Details"
                                     to={`${sessionLocations.promotionFailedList}?sessionClassId=${item.sessionClassId}`}
                                   >
-                                    <Badge bg="danger">{item.totalStudentsFailed}</Badge>
+                                    <Badge bg="danger">
+                                      {item.totalStudentsFailed}
+                                    </Badge>
                                   </Link>
                                 </OverlayTrigger>
                               </td>
                               <td className="h4 text-center">
-                                {item.totalStudentsInClass - item.totalStudentsFailed}</td>
+                                {item.totalStudentsInClass -
+                                  item.totalStudentsFailed}
+                              </td>
                               <td className="h5 text-center">
                                 <div className="form-group">
-                              <select
-                                as="select"
-                                className="form-select"
-                                name="classId"
-                                id="form"
-                                onChange={(e) => setClassToPromoteTo(e.target.value)}
-                              >
-                                <option defaultValue={""}>
-                                  Select promotion class
-                                </option>
-                                {classesToPromoteTo.slice(idx + 1, classesToPromoteTo.length).map((promoteTo, idx) => (
-                                  <option key={idx} value={promoteTo.sessionClassId}>
-                                    {promoteTo.class}
-                                  </option>
-                                ))}
-                              </select>
-                            </div>
+                                  <select
+                                    as="select"
+                                    className="form-select"
+                                    name="classId"
+                                    id="form"
+                                    onChange={(e) =>
+                                      setClassToPromoteTo(e.target.value)
+                                    }
+                                  >
+                                    <option defaultValue={""}>
+                                      Select promotion class
+                                    </option>
+                                    {classesToPromoteTo
+                                      .slice(idx + 1, classesToPromoteTo.length)
+                                      .map((promoteTo, idx) => (
+                                        <option
+                                          key={idx}
+                                          value={promoteTo.sessionClassId}
+                                        >
+                                          {promoteTo.class}
+                                        </option>
+                                      ))}
+                                  </select>
+                                </div>
                               </td>
                               <td>
                                 <div className="flex align-items-center list-user-action">
@@ -223,8 +251,12 @@ console.log('classtopromote',classToPromote)
                                       data-original-title="Details"
                                       to="#"
                                       onClick={() => {
-                                       // promoteStudent(classToPromote, classToPromoteTo)(dispatch);
-                                        setClassToPromote(sessionClassId);
+                                        setClassToPromote(item.sessionClassId);
+                                        const message =
+                                          classToPromoteTo != ""
+                                            ? `Are you sure you want to promote student ?`
+                                            : `Are you sure to unenroll students ?`;
+                                        showHideDialog(true, message)(dispatch);
                                       }}
                                     >
                                       <span className="btn-inner">
@@ -237,7 +269,7 @@ console.log('classtopromote',classToPromote)
                                           <path
                                             fillRule="evenodd"
                                             clipRule="evenodd"
-                                            d='M21.4354 2.58198C20.9352 2.0686 20.1949 1.87734 19.5046 2.07866L3.408 6.75952C2.6797 6.96186 2.16349 7.54269 2.02443 8.28055C1.88237 9.0315 2.37858 9.98479 3.02684 10.3834L8.0599 13.4768C8.57611 13.7939 9.24238 13.7144 9.66956 13.2835L15.4329 7.4843C15.723 7.18231 16.2032 7.18231 16.4934 7.4843C16.7835 7.77623 16.7835 8.24935 16.4934 8.55134L10.72 14.3516C10.2918 14.7814 10.2118 15.4508 10.5269 15.9702L13.6022 21.0538C13.9623 21.6577 14.5826 22 15.2628 22C15.3429 22 15.4329 22 15.513 21.9899C16.2933 21.8893 16.9135 21.3558 17.1436 20.6008L21.9156 4.52479C22.1257 3.84028 21.9356 3.09537 21.4354 2.58198Z'
+                                            d="M21.4354 2.58198C20.9352 2.0686 20.1949 1.87734 19.5046 2.07866L3.408 6.75952C2.6797 6.96186 2.16349 7.54269 2.02443 8.28055C1.88237 9.0315 2.37858 9.98479 3.02684 10.3834L8.0599 13.4768C8.57611 13.7939 9.24238 13.7144 9.66956 13.2835L15.4329 7.4843C15.723 7.18231 16.2032 7.18231 16.4934 7.4843C16.7835 7.77623 16.7835 8.24935 16.4934 8.55134L10.72 14.3516C10.2918 14.7814 10.2118 15.4508 10.5269 15.9702L13.6022 21.0538C13.9623 21.6577 14.5826 22 15.2628 22C15.3429 22 15.4329 22 15.513 21.9899C16.2933 21.8893 16.9135 21.3558 17.1436 20.6008L21.9156 4.52479C22.1257 3.84028 21.9356 3.09537 21.4354 2.58198Z"
                                             stroke="currentColor"
                                             strokeWidth="1.5"
                                             strokeLinecap="round"
