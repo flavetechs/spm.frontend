155a4fd853c99b9d8797b1ee88d6efc32fd8f96d
unknown <you@example.com>
2022-07-22 09:04:46 +0100

    SP-79 feat(smp): SP-79-dashboard

---
+++ src/store/states/dashboard-state.js
@@ -0,0 +1,11 @@
+export const _state = {
+    loading: false,
+    message: "",
+    isSuccessful: false,
+    totalStudents:[],
+    totalStaff:[],
+    totalClasses:[],
+    totalSubjects:[],
+    totalPins:[],
+  };
+  
