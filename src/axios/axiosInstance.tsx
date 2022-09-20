5c69ecfe4b48315274317ce61c4d899b409155dc
flave1 <favouremmanuel433@gmail.com>
2022-09-20 11:46:26 +0100

    .

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    // baseURL: 'http://flavetechs.com/smp/development/',
-    baseURL: 'https://localhost:44373/',
+    baseURL: 'http://flavetechs.com/smp/development/',
+    // baseURL: 'https://localhost:44373/',
     headers: {
         Authorization: '',
     },
