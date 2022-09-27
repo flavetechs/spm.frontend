5e55d71fdc677c4719543ddb00fd4e23a601011f
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2022-09-27 09:45:57 +0100

    SP-111 some-backlog-fix

--- src/components/teachers-area/spm-class/assessment/home-assement-list.js
+++ src/components/teachers-area/spm-class/assessment/home-assement-list.js
@@ -6,7 +6,7 @@ import { closeHomeAssessment } from "../../../../store/actions/class-actions";
 export function HomeAssessmentList(props) {
   const history = useHistory();
   const dispatch = useDispatch();
-  console.log("this",props.assessmentList);
+  console.log("this", props.assessmentList);
   return (
     <>
       <Col md="6" lg="4" xxl="3" className="" key={props.idx}>
@@ -145,44 +145,46 @@ export function HomeAssessmentList(props) {
                       edit
                     </div>
 
-                    <div
-                      onClick={() => {
-                        // props.setHomeAssessmentId(
-                        //     props.item.homeAssessmentId
-                        // );
-                        closeHomeAssessment(
-                          props.item.homeAssessmentId,
-                          props.sessionClassIdQueryParam,
-                          props.selectedSessionClassSubjectId,
-                          props.groupIdQueryParam
-                        )(dispatch);
-                        props.setShowMenuDropdown(false);
-                        // props.showHideDialog(
-                        //     true,
-                        //     "Are yo)u sure you want to delete this assessment"
-                        // )(dispatch);
-                      }}
-                      className="dropdown-item"
-                      role="button"
-                      draggable="false"
-                    >
-                      <svg
-                        width="20"
-                        clipRule="evenodd"
-                        fillRule="evenodd"
-                        strokelinejoin="round"
-                        strokeMiterlimit="2"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
+                    {props.item.status !== "closed" && (
+                      <div
+                        onClick={() => {
+                          // props.setHomeAssessmentId(
+                          //     props.item.homeAssessmentId
+                          // );
+                          closeHomeAssessment(
+                            props.item.homeAssessmentId,
+                            props.sessionClassIdQueryParam,
+                            props.selectedSessionClassSubjectId,
+                            props.groupIdQueryParam
+                          )(dispatch);
+                          props.setShowMenuDropdown(false);
+                          // props.showHideDialog(
+                          //     true,
+                          //     "Are yo)u sure you want to delete this assessment"
+                          // )(dispatch);
+                        }}
+                        className="dropdown-item"
+                        role="button"
+                        draggable="false"
                       >
-                        <path
-                          d="m20 20h-15.25c-.414 0-.75.336-.75.75s.336.75.75.75h15.75c.53 0 1-.47 1-1v-15.75c0-.414-.336-.75-.75-.75s-.75.336-.75.75zm-1-17c0-.478-.379-1-1-1h-15c-.62 0-1 .519-1 1v15c0 .621.52 1 1 1h15c.478 0 1-.379 1-1zm-15.5.5h14v14h-14zm7 5.937 2.219-2.22c.146-.146.339-.219.531-.219.404 0 .75.325.75.75 0 .193-.073.384-.219.531l-2.22 2.22 2.222 2.222c.147.147.22.339.22.53 0 .427-.349.751-.75.751-.192 0-.384-.073-.53-.219l-2.223-2.223-2.223 2.223c-.146.146-.338.219-.53.219-.401 0-.75-.324-.75-.751 0-.191.073-.383.22-.53l2.222-2.222-2.22-2.22c-.146-.147-.219-.338-.219-.531 0-.425.346-.75.75-.75.192 0 .385.073.531.219z"
-                          fillRule="nonzero"
-                        />
-                      </svg>
-                      {" "}close
-                    </div>
+                        <svg
+                          width="20"
+                          clipRule="evenodd"
+                          fillRule="evenodd"
+                          strokelinejoin="round"
+                          strokeMiterlimit="2"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="m20 20h-15.25c-.414 0-.75.336-.75.75s.336.75.75.75h15.75c.53 0 1-.47 1-1v-15.75c0-.414-.336-.75-.75-.75s-.75.336-.75.75zm-1-17c0-.478-.379-1-1-1h-15c-.62 0-1 .519-1 1v15c0 .621.52 1 1 1h15c.478 0 1-.379 1-1zm-15.5.5h14v14h-14zm7 5.937 2.219-2.22c.146-.146.339-.219.531-.219.404 0 .75.325.75.75 0 .193-.073.384-.219.531l-2.22 2.22 2.222 2.222c.147.147.22.339.22.53 0 .427-.349.751-.75.751-.192 0-.384-.073-.53-.219l-2.223-2.223-2.223 2.223c-.146.146-.338.219-.53.219-.401 0-.75-.324-.75-.751 0-.191.073-.383.22-.53l2.222-2.222-2.22-2.22c-.146-.147-.219-.338-.219-.531 0-.425.346-.75.75-.75.192 0 .385.073.531.219z"
+                            fillRule="nonzero"
+                          />
+                        </svg>{" "}
+                        close
+                      </div>
+                    )}
 
                     <div
                       onClick={() => {
