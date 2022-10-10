be517640222273b7392900f41c9630468d98c135
flave1 <favouremmanuel433@gmail.com>
2022-10-10 10:32:41 +0100

    ..

--- src/store/actions/dashboard-actions.js
+++ src/store/actions/dashboard-actions.js
@@ -3,8 +3,7 @@ import axiosInstance from "../../axios/axiosInstance";
 export const getAllDashboardCount = () => {
     axiosInstance.get(`/dashboard/api/v1/get/dashboard-count`)
         .then((res) => {
-           return res?.data?.result
-            sessionStorage.setItem('dashboardData', JSON.stringify(res?.data?.result));
+           return res?.data?.result;
         })
         .catch((err) => {
            return err.response.data.result
