3526f3f9cc1401e889d7e978be6abd06d4d07113
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2023-04-04 19:13:57 +0100

    SP-174-parent-login

--- src/components/students-area/assessment/assessment-details.js
+++ src/components/students-area/assessment/assessment-details.js
@@ -539,7 +539,7 @@ const StudentAssessmentDetails = () => {
                                               fill="currentColor"
                                             ></path>
                                           </svg>
-                                          <p className=" mb-0 text-dark">
+                                          <p className=" mb-0 text-dark"style={{wordBreak: 'break-all', whiteSpace: 'pre-wrap'}}>
                                             {file.split("/")[4]}
                                           </p>
                                         </div>
