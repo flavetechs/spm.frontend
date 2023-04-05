14de0051a2a7bf0ca20367fefea755f27459e650
Oluwaseun-Gbenle <pinkpearl3010@gmail.com>
2023-04-05 18:28:14 +0100

    CBT-17-16-3-23-task

--- src/components/candidate-admission/candidate-edit.js
+++ src/components/candidate-admission/candidate-edit.js
@@ -105,41 +105,18 @@ const CandidateEdit = () => {
 
       React.useEffect(() => {
         let url = singleAdmissionDetail?.photo
-const toDataURL = url => fetch(url)
-      .then(response => response.blob())
-      .then(blob => new Promise((resolve, reject) => {
-      const reader = new FileReader()
-      reader.onloadend = () => resolve(reader.result)
-      reader.onerror = reject
-      reader.readAsDataURL(blob)
-     }))
+       const fileName = 'candidate-photo.jpg'
 
+  fetch(images)
+  .then(async response => {
+    const blob = await response.blob()
+    const file = new File([blob], fileName, {    type: "image/jpeg", })
+    // access file here
 
-// ***Here is code for converting "Base64" to javascript "File Object".***
-
-  function dataURLtoFile(dataurl, filename) {
-     var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-     bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-     while(n--){
-     u8arr[n] = bstr.charCodeAt(n);
-     }
-   return new File([u8arr], filename, {type:mime});
-  }
-
-
-// *** Calling both function ***
-
-  toDataURL(url)
-  .then(dataUrl => {
-  
-     var fileData = dataURLtoFile(dataUrl, "imageName.jpg");
-     console.log("Here is JavaScript File Object",fileData);
-     let fileArr = [];
-     fileArr.push(fileData)
-     setImageFile(fileArr)
+     setImageFile(file)
    })
-      }, [singleAdmissionDetail]);
-   console.log('images', images)
+      }, [images]);
+  // console.log('images', imageFile)
     return (
         <>
           <SmpLoader />
