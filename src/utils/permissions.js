427ed7753892d0fbab77ab70b1ca0922f0f5eb83
flave1 <favouremmanuel433@gmail.com>
2023-06-26 16:02:22 +0100

    as

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -90,3 +90,14 @@ export const getUserDetails = () => {
         localStorage.removeItem('userDetail');
     }
 }
+
+export const getUserDetails2 = async () => {
+    var userDetail = localStorage.getItem('userDetail');
+    if(userDetail !== 'undefined')
+        return JSON.parse(userDetail);
+    else{
+        localStorage.removeItem('token');
+        localStorage.removeItem('permissions');
+        localStorage.removeItem('userDetail');
+    }
+}
