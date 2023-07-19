f26a07f0f8a71b110536eee9b084bef2c07905ba
flave1 <favouremmanuel433@gmail.com>
2023-07-19 09:05:49 +0100

    mm

--- src/utils/other.tsx
+++ src/utils/other.tsx
@@ -3,12 +3,12 @@ export class ServiceURLs {
     //     return 'http://queencollege.flavetechs.com';
     // }
     static GetAppUrl(): string {
-        return process.env.NODE_ENV === "development" ? 'http://portschools.flavetechs.com' : window.location.origin;
+        return process.env.NODE_ENV === "development" ? 'http://futureschools.flaveconsole.com' : window.location.origin;
     }
     static SmpDocumentation(): string {
-        return 'http://fws.flavetechs.com/docs/smp-canvas?feature=';
+        return 'http://fws.flaveconsole.com/docs/smp-canvas?feature=';
     }
     static SmpDocumentation2(): string {    
-        return 'http://fws.flavetechs.com/docs/smp?feature=';
+        return 'http://fws.flaveconsole.com/docs/smp?feature=';
     }
 }
