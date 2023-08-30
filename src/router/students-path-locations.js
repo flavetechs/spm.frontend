a3cb7d51125dc593bfe44a8d9270049954fced42
flave1 <favouremmanuel433@gmail.com>
2023-08-30 07:42:16 +0100

    xz

--- src/router/students-path-locations.js
+++ src/router/students-path-locations.js
@@ -1,57 +1,57 @@
 export const studentDashboardLocations = {
-    dashboard: "/stds-dashboard/",
-  };
-  
-  export const onlineClassLocations = {
-    active:"/stds-dashboard/student-online-class/",
-    onlineClass: "/stds-dashboard/student-online-class/class",
-  };
+  dashboard: "/stds-dashboard/",
+};
 
-  export const studentAssessmentLocations = {
-    active:"/stds-dashboard/smp-student-assessment/",
-    assessment: "/stds-dashboard/smp-student-assessment/assessment",
-    assessmentDetails: "/stds-dashboard/smp-student-assessment/assessment-details",
-  };
+export const onlineClassLocations = {
+  active: "/stds-dashboard/student-online-class/",
+  onlineClass: "/stds-dashboard/student-online-class/class",
+};
 
-  export const studentClassNoteLocations = {
-    active:"/stds-dashboard/smp-student-classnote/",
-    classNotes: "/stds-dashboard/smp-student-classnote/class-note",
-    classNotesDetails:"/stds-dashboard/smp-student-classnote/class-note-details",
-  };
+export const studentAssessmentLocations = {
+  active: "/stds-dashboard/smp-student-assessment/",
+  assessment: "/stds-dashboard/smp-student-assessment/assessment",
+  assessmentDetails: "/stds-dashboard/smp-student-assessment/assessment-details",
+};
 
-  export const studentNoteLocations = {
-    active:"/stds-dashboard/smp-student-studentnote/",
-    studentNotes: "/stds-dashboard/smp-student-studentnote/student-note",
-    createStudentNotes:"/stds-dashboard/smp-student-studentnote/create-student-notes",
-    editStudentNotes:"/stds-dashboard/smp-student-studentnote/edit-student-notes",
-    studentNotesDetails:"/stds-dashboard/smp-student-studentnote/student-note-details",
-  };
+export const studentClassNoteLocations = {
+  active: "/stds-dashboard/smp-student-classnote/",
+  classNotes: "/stds-dashboard/smp-student-classnote/class-note",
+  classNotesDetails: "/stds-dashboard/smp-student-classnote/class-note-details",
+};
 
-  export const studentAnnouncementLocations = {
-    active:"/stds-dashboard/smp-student-announcement/",
-    announcement: "/stds-dashboard/smp-student-announcement/announcement",
-    studentAnnouncementDetails: "/stds-dashboard/smp-student-announcement/announcement-details",
-  };
+export const studentNoteLocations = {
+  active: "/stds-dashboard/smp-student-studentnote/",
+  studentNotes: "/stds-dashboard/smp-student-studentnote/student-note",
+  createStudentNotes: "/stds-dashboard/smp-student-studentnote/create-student-notes",
+  editStudentNotes: "/stds-dashboard/smp-student-studentnote/edit-student-notes",
+  studentNotesDetails: "/stds-dashboard/smp-student-studentnote/student-note-details",
+};
 
-  export const studentTimeTableLocations = {
-    active:"/stds-dashboard/smp-student-timetable/",
-    classTimeTable: "/stds-dashboard/smp-student-timetable/class-time-table",
-    examTimeTable: "/stds-dashboard/smp-student-timetable/exam-time-table",
-    printTimeTable: "/stds-dashboard/smp-student-timetable/print-time-table",
-    cumulativeTimeTable: "/stds-dashboard/smp-student-timetable/cumulative-time-table",
-  };
+export const studentAnnouncementLocations = {
+  active: "/stds-dashboard/smp-student-announcement/",
+  announcement: "/stds-dashboard/smp-student-announcement/announcement",
+  studentAnnouncementDetails: "/stds-dashboard/smp-student-announcement/announcement-details",
+};
 
-  export const studentPrintResultLocations = {
-    active:"/stds-dashboard/smp-student-print-result/",
-    printResult: "/stds-dashboard/smp-student-print-result/print-result",
-    resultTemplate:"/stds-dashboard/smp-student-print-result/result-template",
-  };
+export const studentTimeTableLocations = {
+  active: "/stds-dashboard/smp-student-timetable/",
+  classTimeTable: "/stds-dashboard/smp-student-timetable/class-time-table",
+  examTimeTable: "/stds-dashboard/smp-student-timetable/exam-time-table",
+  printTimeTable: "/stds-dashboard/smp-student-timetable/print-time-table",
+  cumulativeTimeTable: "/stds-dashboard/smp-student-timetable/cumulative-time-table",
+};
 
-  export const studentProfileLocations = {
-    active:"/stds-dashboard/smp-student-profile/",
-    profile: "/stds-dashboard/smp-student-profile/student-profile",
-    profileEdit: "/stds-dashboard/smp-student-profile/edit-student-profile",
-  };
-  export const studentResetPasswordLocations = {
-    resetPassword:"/stds-dashboard/reset-password",
-  }
+export const studentPrintResultLocations = {
+  active: "/stds-dashboard/smp-student-print-result/",
+  printResult: "/stds-dashboard/smp-student-print-result/print-result",
+  resultTemplate: "/stds-dashboard/smp-student-print-result/result-template",
+};
+
+export const studentProfileLocations = {
+  active: "/stds-dashboard/smp-student-profile/",
+  profile: "/stds-dashboard/smp-student-profile/student-profile",
+  profileEdit: "/stds-dashboard/smp-student-profile/edit-student-profile",
+};
+export const studentResetPasswordLocations = {
+  resetPassword: "/stds-dashboard/reset-password",
+}
