2e606fb9ee0768b627ae0396a5d4d962c5903311
flave1 <favouremmanuel433@gmail.com>
2023-09-04 12:47:50 +0100

    gh

--- src/utils/other.tsx
+++ src/utils/other.tsx
@@ -1,11 +1,11 @@
 export class ServiceURLs {
     static GetAppUrl(): string {
-        return process.env.NODE_ENV === "development" ? 'http://futureschools.flaveconsole.com' : window.location.origin;
+        return process.env.NODE_ENV === "development" ? 'https://futureschools.flaveconsole.com' : window.location.origin;
     }
     static SmpDocumentation(): string {
-        return 'http://flaveconsole.com/docs/smp-canvas?feature=';
+        return 'https://flaveconsole.com/docs/smp-canvas?feature=';
     }
     static SmpDocumentation2(): string {    
-        return 'http://flaveconsole.com/docs/smp?feature=';
+        return 'https://flaveconsole.com/docs/smp?feature=';
     }
 }
